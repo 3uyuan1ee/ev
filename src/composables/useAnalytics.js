66631a6ffc57e805fd41f877bc7696f0ec357453
Muy Lee <1481059602@qq.com>
2026-04-22 01:15:22 +0800

    fix：src/composables/useAnalytics.js:11 使用了 crypto.randomUUID()，这个 API 只在 HTTPS（安全上下文）下可用。你的 http://3uyuan1ee.me 是 HTTP，crypto.randomUUID 为 undefined，直接抛出 TypeError 导致整个 Vue 应用在初始化阶段崩溃。

--- src/composables/useAnalytics.js
+++ src/composables/useAnalytics.js
@@ -8,7 +8,9 @@ const STORAGE_KEY = 'ev-session-id'
 function getSessionId() {
   let id = localStorage.getItem(STORAGE_KEY)
   if (!id) {
-    id = crypto.randomUUID()
+    id = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
     localStorage.setItem(STORAGE_KEY, id)
   }
   return id
